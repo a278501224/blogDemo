2af99b94f8b53ffbe61e1c4490fea78e28bd8c8c
a278501224 <yichaotang@qq.com>
2020-09-15 16:49:28 +0800

    添加redis常量池

---
+++ src/main/java/cn/cherry/blog/constant/RedisConstant.java
@@ -0,0 +1,24 @@
+package cn.cherry.blog.constant;
+
+/**
+ * @description: redis相关常量
+ * @author: Cherry
+ * @create: 2020-09-15 10:30
+ **/
+public class RedisConstant {
+    /**
+     * 启用redis标志
+     * */
+    public static final String REDIS_ENABLE_TRUE = "true";
+    /**
+     * 不启用redis标志
+     * */
+    public static final String REDIS_ENABLE_FALSE = "false";
+    /**
+     * 登陆认证TOKEN 前缀
+     * */
+    public static final String REDIS_AUTH_TOKEN_PRE = "AUTH_";
+
+
+
+}
